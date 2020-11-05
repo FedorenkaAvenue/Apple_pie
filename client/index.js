c4a2f07d6af2b8f67c0742dd77e1225f4eee645f
bringmetheaugust <bringmetheaugust@gmail.com>
2020-11-06 01:07:23 +0200

    тестовая страница для auth

--- client/index.js
+++ client/index.js
@@ -1,1 +1,14 @@
-console.log('ебаш, Уитя, ебаш!!');
+document.getElementById('mail').addEventListener('submit', async e => {
+    e.preventDefault();
+
+    await fetch('http://localhost:2700/api/auth/signup', {
+        method: 'POST',
+        body: JSON.stringify({
+            name: document.getElementById('name').value,
+            email: document.getElementById('email').value,
+            password: document.getElementById('password').value,
+            role: document.getElementById('role').value
+        }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+});
--- server/src/app.ts
+++ server/src/app.ts
@@ -8,6 +8,13 @@ const app: Express = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', '*').
+        append('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE').
+        append('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+});
 
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
