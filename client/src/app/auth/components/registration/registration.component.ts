b4c50dff7bed83a7bd4c295517a7ec80e7fa803c
Viktor <viktor.loik@idapgroup.com>
2020-11-12 19:57:17 +0200

    added register form

--- client/src/app/auth/components/registration/registration.component.ts
+++ client/src/app/auth/components/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'app/auth/services/login/auth.service';
 
 @Component({
   selector: 'app-registration',
@@ -7,13 +8,31 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
-  button: string = 'buttons.registration';
-  private _prefix: string = 'auth.registration.form.';
-  form: FormGroup;
+  public button: string = 'buttons.registration';
+  public form: FormGroup;
 
-  constructor(private _formBuilder: FormBuilder) {
+  constructor(
+    private readonly _fb: FormBuilder,
+    private readonly _authService: AuthService
+    ) {
   }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.form = this._fb.group({
+      name: this._fb.control(null, [Validators.required]),
+      password: this._fb.control(null, [Validators.required]),
+      email: this._fb.control(null, [Validators.required]),
+      role: this._fb.control(null, [Validators.required]),
+    });
+  }
+
+  public onSubmit(): void {
+    this._authService.signUp(this.form.value).subscribe(el => {
+      console.log(el);
+    })
   }
 }
