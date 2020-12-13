6b9ae60f5b51d8774c9a631af223e3b3f405e69c
Viktor <viktor.loik@idapgroup.com>
2020-12-13 23:40:44 +0200

    some

--- client/src/app/shared/layouts/layouts.module.ts
+++ client/src/app/shared/layouts/layouts.module.ts
@@ -7,10 +7,13 @@ import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { WrapperComponent } from './wrapper/wrapper.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, TranslateModule],
+  imports: [CommonModule, RouterModule, TranslateModule, MatToolbarModule, MatIconModule],
   declarations: [FooterComponent, SidebarComponent, ToolbarComponent, WrapperComponent],
   exports: [FooterComponent, SidebarComponent, ToolbarComponent, WrapperComponent],
 })
-export class LayoutsModule {}
+export class LayoutsModule { }
+
