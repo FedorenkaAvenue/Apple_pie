638bacba00bf857483758e4e9f52ab2210a2e5d4
Viktor <viktor.loik@idapgroup.com>
2020-12-17 15:42:11 +0200

    remove shared module

--- client/src/app/auth/components/registration/registration.component.ts
+++ client/src/app/auth/components/registration/registration.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { UserType } from '@shared/enums/user.enum';
 import { AuthService } from 'app/auth/services/login/auth.service';
 
 @Component({
@@ -10,14 +12,17 @@ import { AuthService } from 'app/auth/services/login/auth.service';
 export class RegistrationComponent implements OnInit {
   public button: string = 'buttons.registration';
   public form: FormGroup;
+  public userRole: UserType;
 
   constructor(
     private readonly _fb: FormBuilder,
-    private readonly _authService: AuthService
+    private readonly _authService: AuthService,
+    private readonly _route: ActivatedRoute
     ) {
   }
 
   ngOnInit() {
+    this.userRole = +this._route.snapshot.paramMap.get('type');
     this.initForm();
   }
 
@@ -26,13 +31,12 @@ export class RegistrationComponent implements OnInit {
       name: this._fb.control(null, [Validators.required]),
       password: this._fb.control(null, [Validators.required]),
       email: this._fb.control(null, [Validators.required]),
-      role: this._fb.control(null, [Validators.required]),
     });
   }
 
   public onSubmit(): void {
-    this._authService.signUp(this.form.value).subscribe(el => {
+    this._authService.signUp({...this.form.value, role: this.userRole}).subscribe(el => {
       console.log(el);
-    })
+    });
   }
 }
