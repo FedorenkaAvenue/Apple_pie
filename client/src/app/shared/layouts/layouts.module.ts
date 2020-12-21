1aa992dcd7535d25f9923f117121a2dfa17f6bfe
Viktor <viktor.loik@idapgroup.com>
2020-12-21 22:03:21 +0200

    added toolbar

--- client/src/app/shared/layouts/layouts.module.ts
+++ client/src/app/shared/layouts/layouts.module.ts
@@ -7,9 +7,8 @@ import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { WrapperComponent } from './wrapper/wrapper.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
   imports: [CommonModule, RouterModule, TranslateModule, MatToolbarModule, MatIconModule],
   declarations: [FooterComponent, SidebarComponent, ToolbarComponent, WrapperComponent],
