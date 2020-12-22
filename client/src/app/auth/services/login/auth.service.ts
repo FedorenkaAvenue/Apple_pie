20b7dee0792c0641643142416db9e6d598dc79fb
Viktor <viktor.loik@idapgroup.com>
2020-12-22 20:03:51 +0200

    added local storage service

--- client/src/app/auth/services/login/auth.service.ts
+++ client/src/app/auth/services/login/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { TransferHttpService } from '@gorniv/ngx-universal';
+import { LocalStorageService } from '@shared/services/local-storage/local-storage.service';
+import { TokenService } from '@shared/services/token/token.service';
 import { Observable, of } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 
@@ -11,11 +13,19 @@ export class AuthService {
   private readonly apiUrl = '/api/sign';
 
   constructor(
-    private _http: TransferHttpService
+    private _http: TransferHttpService,
+    private _tokenService: TokenService,
+    private _localStorageService: LocalStorageService
   ) { }
 
   signUp(signUpDTO: any): Observable<any> {
-    return this._http.post(`${this.apiUrl}/signup`, signUpDTO);
+    return this._http.post(`${this.apiUrl}/signup`, signUpDTO)
+    .pipe(
+      tap(token => {
+        this._localStorageService.setItem('auth-token', token);
+        this._tokenService.setToken(token);
+      })
+    )
   }
 
   signIn(signInDTO: any): Observable<any> {
