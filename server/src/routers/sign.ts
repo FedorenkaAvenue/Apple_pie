b1fec76b7029f80fc5287b450fa7f4d50967f858
bringmetheaugust <bringmetheaugust@gmail.com>
2021-01-11 23:26:48 +0200

    раздельный роут для аутентификаций через почту и соц сети

--- server/src/routers/sign.ts
+++ server/src/routers/sign.ts
@@ -1,14 +1,18 @@
 import { Router } from 'express';
 
-import signUpController from '@controllers/sign/signUp';
-import signInController from '@controllers/sign/signIn';
+import signUpEmailController from '@controllers/sign/up/email';
+import signUpSocController from '@controllers/sign/up/cosial';
+
+import signInEmailController from '@controllers/sign/in/email';
+
 import signOutController from '@controllers/sign/signOut';
 
 const router: Router = Router();
 
 router
-    .post('/up', signUpController)
-    .post('/in', signInController)
+    .post('/up/soc', signUpSocController)
+    .post('/up', signUpEmailController)
+    .post('/in', signInEmailController)
     .get('/out', signOutController);
 
 export default router;
