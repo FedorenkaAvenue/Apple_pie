c94396cb1ba9ebcfc5de5b026c43723f401eda8b
bringmetheaugust <bringmetheaugust@gmail.com>
2021-01-12 23:45:59 +0200

    разделил роуты для почти и соц сетей

--- server/src/routers/sign.ts
+++ server/src/routers/sign.ts
@@ -1,18 +1,26 @@
 import { Router } from 'express';
 
 import signUpEmailController from '@controllers/sign/up/email';
-import signUpSocController from '@controllers/sign/up/cosial';
-
+import signUpGoogleController from '@controllers/sign/up/google';
 import signInEmailController from '@controllers/sign/in/email';
-
+import signInGoogleController from '@controllers/sign/in/google';
 import signOutController from '@controllers/sign/signOut';
 
-const router: Router = Router();
+const router = Router();
+const signUpRouter = Router();
+const signInRouter = Router();
+
+signUpRouter
+    .post('/google', signUpGoogleController)
+    .post('/email', signUpEmailController);
+
+signInRouter
+    .post('/google', signInGoogleController)
+    .post('/email', signInEmailController)
 
 router
-    .post('/up/soc', signUpSocController)
-    .post('/up', signUpEmailController)
-    .post('/in', signInEmailController)
+    .use('/up', signUpRouter)
+    .use('/in', signInRouter)
     .get('/out', signOutController);
 
 export default router;
