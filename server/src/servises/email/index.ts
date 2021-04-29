efba3173a17fb752139ed9772919c5c36d7662cb
bringmetheaugust <bringmetheaugust@gmail.com>
2021-04-29 17:55:29 +0300

    fix SMTP connection

--- server/src/servises/email/index.ts
+++ server/src/servises/email/index.ts
@@ -1,16 +1,15 @@
 import { createTransport } from 'nodemailer';
 
-const { MAIL_ACCOUNT, MAIL_PASSWORD, MAIL_SMTP_HOST, MAIL_SMTP_PORT } = process.env;
+const { MAIL_SMTP_HOST, MAIL_SMTP_PORT } = process.env;
 
 export const emailTransporter = createTransport({
     host: MAIL_SMTP_HOST,
     secure: false,
     port: Number(MAIL_SMTP_PORT),
-    auth: {
-        user: MAIL_ACCOUNT,
-        pass: MAIL_PASSWORD
-    },
-    tls: { rejectUnauthorized: false }
+    // auth: {
+    //     user: 'support',
+    //     pass: MAIL_PASSWORD
+    // }
 });
 
 // DEV: test SMTP server connection
